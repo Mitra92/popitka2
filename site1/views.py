f72d2f596a92e1c7e761dcc641b038a6087e2668
Mitra92 <craz93@yandex.ru>
2018-03-16 00:12:30 +0300

    popitka2.2

--- site1/views.py
+++ site1/views.py
@@ -4,5 +4,5 @@ from .models import Post
 
 
 def post_list(request):
-    posts = Post.objects.filter()
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'site1/post_list.html', {'posts':posts})
